ccd1bfbeeed8a2b1f48cb9d54242716ae66b11bc
Tupi Guedes <tupiguedes@outlook.com>
2021-07-29 10:07:28 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-
-		System.out.println("Olá mundo!");
+		Position position = new Position(10, 20);
+		System.out.println(position);
 	}
 
 }
